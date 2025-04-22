55456601d31dea6253e391cc2fb3adf099facad2
Rodrigo55pereira <rodrigo55pereira.com>
2025-04-22 14:47:56 -0300

    adicao do box component

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -2,6 +2,7 @@ import {TouchableOpacity, View} from 'react-native';
 import {Text} from '../Text/Text';
 import { useTheme } from '@shopify/restyle'
 import { Theme } from '../../theme/theme'
+import { Box } from '../Box/Box'
 
 interface ButtonProps {
   title: string;
@@ -10,15 +11,18 @@ interface ButtonProps {
 export const Button = ({title}: ButtonProps) => {
   const { colors } = useTheme<Theme>();
   return (
-    <TouchableOpacity
-      style={{
-        paddingHorizontal: 20,
-        paddingVertical: 14,
-        backgroundColor: colors.carrotSecondary,
-        alignItems: 'center',
-        borderRadius: 16,
-      }}>
+    <Box
+      backgroundColor='primary'
+      paddingHorizontal='s20'
+      // style={{
+      //   paddingHorizontal: 20,
+      //   paddingVertical: 14,
+      //   backgroundColor: colors.carrotSecondary,
+      //   alignItems: 'center',
+      //   borderRadius: 16,
+      // }}
+      >
       <Text preset="paragraphMedium" bold style={{color: 'white'}}>{title}</Text>
-    </TouchableOpacity>
+    </Box>
   );
 };
--- src/theme/theme.ts
+++ src/theme/theme.ts
@@ -37,6 +37,18 @@ export const theme = createTheme({
     successLight: pallete.greenSuccessLight
   },
   spacing: {
+    s4: 4,
+    s8: 8,
+    s10: 10,
+    s12: 12,
+    s14: 14,
+    s16: 16,
+    s20: 20,
+    s24: 24,
+    s32: 32,
+    s40: 40,
+    s48: 48,
+    s56: 56,
   },
   borderRadii: {
 
